defa17ff66c7e628ee51a3240019eb5e6c94be92
yuyu0317 <zhuyu4010qq@gmail.com>
2025-01-28 00:21:01 +0800

    style: resolve Black and Flake8 rule conflicts

--- check_job.py
+++ check_job.py
@@ -4,14 +4,13 @@ import json
 token = "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"
 headers = {
     "Authorization": f"Bearer {token}",
-    "Accept": "application/vnd.github.v3+json"
+    "Accept": "application/vnd.github.v3+json",
 }
 
 run_id = "12993153863"
-url = (
-    f"https://api.github.com/repos/yuyu0317/python-serverless-demo/actions/runs/{run_id}/jobs"
-)
+base_url = "https://api.github.com/repos/yuyu0317/python-serverless-demo"
+url = f"{base_url}/actions/runs/{run_id}/jobs"
 response = requests.get(url, headers=headers)
 data = response.json()
 
-print(json.dumps(data, indent=2)) 
+print(json.dumps(data, indent=2))
